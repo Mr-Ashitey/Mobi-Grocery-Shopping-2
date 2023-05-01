02e7e6aeeb9a75a9c06bb2ab5b8315151644f3ba
Mr-Ashitey <aamarh17@gmail.com>
2023-05-01 13:41:06 +0000

    setup properties and map to super(entity) properties

--- lib/features/grocery_list/data/model/grocery_list_item_model.dart
+++ lib/features/grocery_list/data/model/grocery_list_item_model.dart
@@ -6,14 +6,26 @@ part 'grocery_list_item_model.g.dart';
 
 @JsonSerializable(explicitToJson: true)
 class GroceryListItemModel extends GroceryListItemEntity {
+  @JsonKey(fromJson: _fromJsonId)
+  int groceryListItemId;
+  String groceryListItemName;
+  bool groceryListItemIsCollected;
+
   GroceryListItemModel({
-    required int id,
-    required String name,
-    bool isCollected = false,
-  }) : super(id: id, name: name, isCollected: isCollected);
+    required this.groceryListItemId,
+    required this.groceryListItemName,
+    this.groceryListItemIsCollected = false,
+  }) : super(
+            id: groceryListItemId,
+            name: groceryListItemName,
+            isCollected: groceryListItemIsCollected);
 
   factory GroceryListItemModel.fromJson(Map<String, dynamic> json) =>
       _$GroceryListItemModelFromJson(json);
 
   Map<String, dynamic> toJson() => _$GroceryListItemModelToJson(this);
+  
+  static int _fromJsonId(dynamic id) {
+    return (id as num).toInt();
+  }
 }
--- lib/features/grocery_list/data/model/grocery_list_model.dart
+++ lib/features/grocery_list/data/model/grocery_list_model.dart
@@ -5,17 +5,28 @@ import 'grocery_list_item_model.dart';
 
 part 'grocery_list_model.g.dart';
 
-@JsonSerializable()
+@JsonSerializable(explicitToJson: true)
 class GroceryListModel extends GroceryListEntity {
+  @JsonKey(fromJson: _fromJsonId)
+  int groceryListId;
+  String groceryListName;
+  List<GroceryListItemModel> groceryListItemsModel;
+
   GroceryListModel({
-    required int id,
-    required String name,
-    List<GroceryListItemModel> groceryListItems =
-        const <GroceryListItemModel>[],
-  }) : super(id: id, name: name, groceryListItems: groceryListItems);
+    required this.groceryListId,
+    required this.groceryListName,
+    this.groceryListItemsModel = const <GroceryListItemModel>[],
+  }) : super(
+            id: groceryListId,
+            name: groceryListName,
+            groceryListItems: groceryListItemsModel);
 
   factory GroceryListModel.fromJson(Map<String, dynamic> json) =>
       _$GroceryListModelFromJson(json);
 
   Map<String, dynamic> toJson() => _$GroceryListModelToJson(this);
+
+  static int _fromJsonId(dynamic id) {
+    return (id as num).toInt();
+  }
 }
