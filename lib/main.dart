69ed7014cd60a33b29108d9f404954da22e5b029
Mr-Ashitey <aamarh17@gmail.com>
2023-05-03 23:17:54 +0000

    - initialize get_it - initialize flutter dotenv - call in the provider - setup go router (routerConfig)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mobi_grocery_shopping_2/config/router.dart';
+import 'package:mobi_grocery_shopping_2/config/theme.dart';
+import 'package:mobi_grocery_shopping_2/features/grocery_list/presentation/provider/grocery_manager.dart';
+import 'package:mobi_grocery_shopping_2/injection_container.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initGetIt();
+  await dotenv.load();
   runApp(const GroceryListApp());
 }
 
@@ -10,13 +19,14 @@ class GroceryListApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'GroceryListApp',
-      debugShowCheckedModeBanner: false,
-      routerConfig: router,
-      // routerDelegate: router.routerDelegate,
-      // routeInformationParser: router.routeInformationParser,
-      // routeInformationProvider: router.routeInformationProvider,
+    return ChangeNotifierProvider.value(
+      value: locator.get<GroceryManager>(),
+      child: MaterialApp.router(
+        title: 'GroceryListApp',
+        debugShowCheckedModeBanner: false,
+        theme: customTheme,
+        routerConfig: router,
+      ),
     );
   }
 }
