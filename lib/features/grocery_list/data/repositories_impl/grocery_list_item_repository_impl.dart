9126f180c414c088d2501e20731df6c48df5abff
Mr-Ashitey <aamarh17@gmail.com>
2023-05-03 18:56:00 +0000

    refactor repository implementations

--- lib/features/grocery_list/data/repositories_impl/grocery_list_item_repository_impl.dart
+++ lib/features/grocery_list/data/repositories_impl/grocery_list_item_repository_impl.dart
@@ -5,82 +5,41 @@ import 'package:dartz/dartz.dart';
 import 'package:mobi_grocery_shopping_2/features/grocery_list/domain/repositories/grocery_list_item_repository.dart';
 
 import '../../../../core/network/network_info.dart';
-import '../datasources/grocery_list_item_datasource/grocery_list_item_local_datasource.dart';
-import '../datasources/grocery_list_item_datasource/grocery_list_item_remote_datasource.dart';
+import '../datasources/grocery_list_item_remote_datasource.dart';
 
 class GroceryListItemRepositoryImpl extends GroceryListItemRepository {
-  final GrcoeryListItemLocalDataSource groceryListItemLocalDataSource;
   final GrcoeryListItemRemoteDataSource groceryListItemRemoteDataSource;
   final NetworkInfo networkInfo;
 
   GroceryListItemRepositoryImpl(
-      {required this.groceryListItemLocalDataSource,
-      required this.groceryListItemRemoteDataSource,
+      {required this.groceryListItemRemoteDataSource,
       required this.networkInfo});
   @override
   Future<Either<Failure, void>> addGroceryListItem(
-      int groceryListId, GroceryListItemEntity groceryListItem) async {
+      GroceryListItemEntity groceryListItem) async {
     try {
       if (await networkInfo.isConnected) {
-        await groceryListItemRemoteDataSource.addGroceryListItem(
-            groceryListId, groceryListItem as GroceryListItemModel);
-        await groceryListItemLocalDataSource.addGroceryListItem(
-            groceryListId, groceryListItem);
+        await groceryListItemRemoteDataSource
+            .addGroceryListItem(groceryListItem as GroceryListItemModel);
+        // await groceryListItemLocalDataSource.addGroceryListItem(
+        //     groceryListId, groceryListItem);
         return const Right(null);
       }
-      return const Right(null);
+      return Left(Failure("No Internet"));
     } catch (e) {
       return Left(Failure(e.toString()));
     }
   }
 
   @override
-  Future<Either<Failure, void>> deleteGroceryListItem(
-      int groceryListId, int id) async {
+  Future<Either<Failure, void>> deleteGroceryListItem(int id) async {
     try {
       if (await networkInfo.isConnected) {
-        await groceryListItemRemoteDataSource.deleteGroceryListItem(
-            groceryListId, id);
-        await groceryListItemLocalDataSource.deleteGroceryListItem(
-            groceryListId, id);
+        await groceryListItemRemoteDataSource.deleteGroceryListItem(id);
+        // await groceryListItemLocalDataSource.deleteGroceryListItem(id);
         return const Right(null);
       }
-      return const Right(null);
-    } catch (e) {
-      return Left(Failure(e.toString()));
-    }
-  }
-
-  @override
-  Future<Either<Failure, GroceryListItemEntity>> getGroceryListItem(
-      int groceryListId, int id) async {
-    try {
-      if (await networkInfo.isConnected) {
-        final groceryListModel = await groceryListItemRemoteDataSource
-            .getGroceryListItem(groceryListId, id);
-
-        return Right(groceryListModel);
-      }
-      final groceryListModel = await groceryListItemLocalDataSource
-          .getGroceryListItem(groceryListId, id);
-      return Right(groceryListModel);
-    } catch (e) {
-      return Left(Failure(e.toString()));
-    }
-  }
-
-  @override
-  Future<Either<Failure, List<GroceryListItemEntity>>> getGroceryListItems(
-      int groceryListId) async {
-    try {
-      if (await networkInfo.isConnected) {
-        final groceryLists = await groceryListItemRemoteDataSource
-            .getGroceryListItems(groceryListId);
-        return Right(groceryLists);
-      }
-      final groceryLists = await groceryListItemLocalDataSource
-          .getGroceryListItems(groceryListId);
-      return Right(groceryLists);
+      return Left(Failure("No Internet"));
     } catch (e) {
       return Left(Failure(e.toString()));
     }
@@ -88,16 +47,16 @@ class GroceryListItemRepositoryImpl extends GroceryListItemRepository {
 
   @override
   Future<Either<Failure, void>> updateGroceryListItem(
-      int groceryListId, int id, GroceryListItemEntity groceryListItem) async {
+      GroceryListItemEntity groceryListItem) async {
     try {
       if (await networkInfo.isConnected) {
-        await groceryListItemRemoteDataSource.updateGroceryListItem(
-            groceryListId, id, groceryListItem as GroceryListItemModel);
-        await groceryListItemLocalDataSource.updateGroceryListItem(
-            groceryListId, groceryListItem.id, groceryListItem);
+        await groceryListItemRemoteDataSource
+            .updateGroceryListItem(groceryListItem as GroceryListItemModel);
+        // await groceryListItemLocalDataSource.updateGroceryListItem(
+        //     groceryListId, groceryListItem.id, groceryListItem);
         return const Right(null);
       }
-      return const Right(null);
+      return Left(Failure("No Internet"));
     } catch (e) {
       return Left(Failure(e.toString()));
     }
--- lib/features/grocery_list/data/repositories_impl/grocery_list_repository_impl.dart
+++ lib/features/grocery_list/data/repositories_impl/grocery_list_repository_impl.dart
@@ -1,20 +1,17 @@
 import 'package:mobi_grocery_shopping_2/core/network/network_info.dart';
-import 'package:mobi_grocery_shopping_2/features/grocery_list/data/datasources/grocery_list_datasource/grocery_list_local_datasource.dart';
 import 'package:mobi_grocery_shopping_2/features/grocery_list/data/model/grocery_list_model.dart';
 import 'package:mobi_grocery_shopping_2/features/grocery_list/domain/entities/grocery_list_entity.dart';
 import 'package:mobi_grocery_shopping_2/core/error/failure.dart';
 import 'package:dartz/dartz.dart';
 import 'package:mobi_grocery_shopping_2/features/grocery_list/domain/repositories/grocery_list_repository.dart';
 
-import '../datasources/grocery_list_datasource/grocery_list_remote_datasource.dart';
+import '../datasources/grocery_list_remote_datasource.dart';
 
 class GroceryListRepositoryImpl extends GroceryListRepository {
-  final GroceryListLocalDataSource groceryListLocalDataSource;
   final GroceryListRemoteDataSource groceryListRemoteDataSource;
   final NetworkInfo networkInfo;
 
   GroceryListRepositoryImpl({
-    required this.groceryListLocalDataSource,
     required this.groceryListRemoteDataSource,
     required this.networkInfo,
   });
@@ -25,10 +22,9 @@ class GroceryListRepositoryImpl extends GroceryListRepository {
       if (await networkInfo.isConnected) {
         await groceryListRemoteDataSource
             .addGroceryList(groceryList as GroceryListModel);
-        await groceryListLocalDataSource.addGroceryList(groceryList);
         return const Right(null);
       }
-      return const Right(null);
+      return Left(Failure('No Internet'));
     } catch (e) {
       return Left(Failure(e.toString()));
     }
@@ -39,27 +35,9 @@ class GroceryListRepositoryImpl extends GroceryListRepository {
     try {
       if (await networkInfo.isConnected) {
         await groceryListRemoteDataSource.deleteGroceryList(id);
-        await groceryListLocalDataSource.deleteGroceryList(id);
         return const Right(null);
       }
-      return const Right(null);
-    } catch (e) {
-      return Left(Failure(e.toString()));
-    }
-  }
-
-  @override
-  Future<Either<Failure, GroceryListEntity>> getGroceryList(int id) async {
-    try {
-      if (await networkInfo.isConnected) {
-        final groceryListModel =
-            await groceryListRemoteDataSource.getGroceryList(id);
-
-        return Right(groceryListModel);
-      }
-      final groceryListModel =
-          await groceryListLocalDataSource.getGroceryList(id);
-      return Right(groceryListModel);
+      return Left(Failure('No Internet'));
     } catch (e) {
       return Left(Failure(e.toString()));
     }
@@ -73,8 +51,7 @@ class GroceryListRepositoryImpl extends GroceryListRepository {
             await groceryListRemoteDataSource.getGroceryLists();
         return Right(groceryLists);
       }
-      final groceryLists = await groceryListLocalDataSource.getGroceryLists();
-      return Right(groceryLists);
+      return const Right(<GroceryListModel>[]);
     } catch (e) {
       return Left(Failure(e.toString()));
     }
@@ -87,10 +64,9 @@ class GroceryListRepositoryImpl extends GroceryListRepository {
       if (await networkInfo.isConnected) {
         await groceryListRemoteDataSource.updateGroceryList(
             id, groceryList as GroceryListModel);
-        await groceryListLocalDataSource.updateGroceryList(id, groceryList);
         return const Right(null);
       }
-      return const Right(null);
+      return Left(Failure('No Internet'));
     } catch (e) {
       return Left(Failure(e.toString()));
     }
