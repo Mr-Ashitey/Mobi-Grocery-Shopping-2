da1277432b32260d5977833387e366ad42e5ccdd
Mr-Ashitey <aamarh17@gmail.com>
2023-05-03 18:25:49 +0000

    finish up splash screen and navigate to home screen after 3 secs

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:mobi_grocery_shopping_2/config/route_path.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,7 +13,51 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    navigateToHomeScreenAfter3Seconds();
+    super.initState();
+  }
+
+  // function to display splash screen for 3 seconds
+  void navigateToHomeScreenAfter3Seconds() {
+    Timer(const Duration(seconds: 3), () async {
+      context.go(RoutePath.homeRoutePath);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: TweenAnimationBuilder(
+          tween: Tween<double>(begin: 0, end: 1),
+          duration: const Duration(milliseconds: 500),
+          builder: (BuildContext context, double size, Widget? child) {
+            return Transform.scale(
+              scale: size,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(
+                    Icons.shopping_basket_rounded,
+                    size: 90,
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "MOBI GROCERY SHOPPING",
+                    style: TextStyle(
+                      fontSize: 20,
+                      letterSpacing: 2,
+                      wordSpacing: 2,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
