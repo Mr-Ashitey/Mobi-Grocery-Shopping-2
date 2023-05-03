e9b9a2bd67f44dfde10edba92b8d588dd7c90ef5
Mr-Ashitey <aamarh17@gmail.com>
2023-05-03 23:31:40 +0000

    add custom reusable progress indicator helper

--- lib/helpers/alert/show_progress_dialog.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-void showProgressDialog(BuildContext context) {
-  showDialog(
-      context: context,
-      builder: (_) {
-        return const CircularProgressIndicator.adaptive();
-      });
-}
