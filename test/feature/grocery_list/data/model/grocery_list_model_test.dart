5f9cd958a47da917f4c77466c4de7ffdf60168e1
Mr-Ashitey <aamarh17@gmail.com>
2023-05-04 20:10:26 +0000

    write more tests to cover entire models

--- test/feature/grocery_list/data/model/grocery_list_model_test.dart
+++ test/feature/grocery_list/data/model/grocery_list_model_test.dart
@@ -39,12 +39,12 @@ void main() {
   });
 
   test(
-      'GroceryListModel should be able to be created from JSON and should pass evene when groceryListItems is empty',
+      'GroceryListModel should be able to be created from JSON and should pass even when groceryListItems is empty',
       () {
     final json = {
       'id': 1,
       'name': 'My Grocery List',
-      'groceryListItems': [],
+      'grocery_list_item': [],
     };
     final groceryListModel = GroceryListModel.fromJson(json);
 
@@ -52,20 +52,37 @@ void main() {
     expect(groceryListModel.name, 'My Grocery List');
     expect(groceryListModel.groceryListItems.length, 0);
   });
-  // test(
-  //     'GroceryListModel should be able to be created from JSON and should convert string id to int',
-  //     () {
-  //   final json = {
-  //     'id': '1',
-  //     'name': 'My Grocery List',
-  //     'groceryListItems': [],
-  //   };
-  //   final groceryListModel = GroceryListModel.fromJson(json);
 
-  //   expect(groceryListModel.id, 1);
-  //   expect(groceryListModel.name, 'My Grocery List');
-  //   expect(groceryListModel.groceryListItems.length, 2);
-  // });
+  test("getter methods should return right values", () {
+    final json = {
+      'id': '1',
+      'name': 'My Grocery List',
+      'grocery_list_item': [
+        {"id": 1, "name": "Grocery List Item", "collected": true},
+        {"id": 2, "name": "Grocery List Item", "collected": false}
+      ],
+    };
+
+    final groceryListModel = GroceryListModel.fromJson(json);
+
+    expect(groceryListModel.numItems, 2);
+    expect(groceryListModel.numItemsCollected, 1);
+    expect(groceryListModel.numItemsNotCollected, 1);
+  });
+  test(
+      'GroceryListModel should be able to be created from JSON and should convert string id to int',
+      () {
+    final json = {
+      'id': '1',
+      'name': 'My Grocery List',
+      'grocery_list_item': [],
+    };
+    final groceryListModel = GroceryListModel.fromJson(json);
+
+    expect(groceryListModel.id, 1);
+    expect(groceryListModel.name, 'My Grocery List');
+    expect(groceryListModel.groceryListItems.length, 0);
+  });
   test(
       'GroceryListModel should be able to be created from JSON with right property types',
       () {
