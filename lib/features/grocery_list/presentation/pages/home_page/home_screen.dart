a7581ad2d5095608f6ee48e7e8b0adb1a8011729
Mr-Ashitey <aamarh17@gmail.com>
2023-05-04 09:21:27 +0000

    change futurebuilder implementation add reload/refresh button to re-pull grocery lists from backend

--- lib/features/grocery_list/presentation/pages/home_page/home_screen.dart
+++ lib/features/grocery_list/presentation/pages/home_page/home_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:mobi_grocery_shopping_2/features/grocery_list/presentation/pages/home_page/components/grocery_list_card.dart';
 import 'package:mobi_grocery_shopping_2/features/grocery_list/presentation/provider/grocery_manager.dart';
+import 'package:mobi_grocery_shopping_2/helpers/alert/custom_progress_dialog.dart';
+import 'package:mobi_grocery_shopping_2/helpers/alert/show_snack_alert.dart';
 import 'package:provider/provider.dart';
 
-import '../../../../../helpers/alert/custom_progress_dialog.dart';
 import 'components/dialog_component/add_grocery_list_dialog.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -15,14 +17,19 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   // Create instance variables.
-  late final Future getGroceryListsFuture;
+  Future? getGroceryListsFuture;
   late TextEditingController controller;
 
   @override
   void initState() {
     super.initState();
     controller = TextEditingController();
-    getGroceryListsFuture = context.read<GroceryManager>().getGroceryLists();
+    fetchGroceryLists();
+  }
+
+  @override
+  void didChangeDependencies() async {
+    super.didChangeDependencies();
   }
 
   @override
@@ -31,38 +38,44 @@ class _HomeScreenState extends State<HomeScreen> {
     super.dispose();
   }
 
+  Future<void> fetchGroceryLists() async {
+    try {
+      // wait for widget tree to be fully built before we call show progress dialog
+      Future.delayed(Duration.zero, () {
+        showProgressDialog(context);
+      });
+      await context.read<GroceryManager>().getGroceryLists();
+
+      // ignore: use_build_context_synchronously
+      context.pop();
+    } catch (error) {
+      context.pop();
+      showNotification(
+          context: context, message: error.toString(), isError: true);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final groceryLists = context.watch<GroceryManager>().groceryLists;
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Grocery Lists')),
-      body: FutureBuilder(
-        future: context.read<GroceryManager>().getGroceryLists(),
-        // future: getGroceryListsFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CustomProgressIndicator();
-          }
-
-          if (snapshot.hasError) {
-            return Center(
-              child: Text(
-                snapshot.error.toString(),
-                style: const TextStyle(fontSize: 30),
-              ),
-            );
-          }
-
-          final groceryLists = context.watch<GroceryManager>().groceryLists;
-          return ListView.builder(
-            reverse: true,
-            shrinkWrap: true,
-            itemCount: groceryLists.length,
-            itemBuilder: (context, index) {
-              final groceryList = groceryLists[index];
-              return GroceryListCard(
-                  groceryList: groceryList, controller: controller);
-            },
-          );
+      appBar: AppBar(
+        title: const Text('Grocery Lists'),
+        actions: [
+          IconButton(
+              onPressed: () => fetchGroceryLists(),
+              icon: const Icon(Icons.refresh))
+        ],
+      ),
+      body: ListView.builder(
+        reverse: true,
+        shrinkWrap: true,
+        itemCount: groceryLists.length,
+        itemBuilder: (context, index) {
+          final groceryList = groceryLists[index];
+          return GroceryListCard(
+              groceryList: groceryList, controller: controller);
         },
       ),
       floatingActionButton: FloatingActionButton(
